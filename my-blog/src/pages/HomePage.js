8a370c48c69ea0693d66a4ffb09d30ae6ea7305d
tjv255 <tjv255@mail.usask.ca>
2022-07-12 11:19:02 -0600

    changed name of frontend directory

---
+++ my-blog/src/pages/HomePage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Hello, welcome to my blog!</h1>
+      <p>Welcome to my blog! I am not a real blogger. Everything in all of my blogs is faked.
+         Instead of going out into the world and sharing my experiences, I will be sharing 
+         pretend experiences that I wish I could experience, but I am too busy learning how 
+         to create web applications. Nonetheless, please follow my page... if that function 
+         ever gets programmed in. 
+      </p>
+      <p>Today I continue to write BS blog entries and... actually, this entry is written 
+        seconds after the first post of the blog. I am just tasked to enter multiple blog 
+        entries so I can work to make them display nicely together. I have nothing else to 
+        share. Sorry!
+      </p>
+    </>
+  )
+}
+
+export default HomePage
