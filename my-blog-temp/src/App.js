cbf7ddc88f759c26d75e6b6fca72f7b9e0c34de2
tjv255 <tjv255@mail.usask.ca>
2022-07-12 11:14:16 -0600

    added files for finished simple blog

---
+++ my-blog-temp/src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import ArticlesListPage from './pages/ArticlesListPage'
+import ArticlePage from './pages/ArticlePage'
+import NotFoundPage from './pages/NotFoundPage'
+import NavBar from './NavBar'
+import './App.css';
+
+function App() {
+  return (
+    
+    <Router>
+      <div className='App'>
+        <NavBar />
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage/>} exact /> 
+            <Route path="/about" element={<AboutPage/>} />
+            <Route path="/articles-list" element={<ArticlesListPage/>} />
+            <Route path="/article/:name" element={<ArticlePage/>} />
+            <Route path="*" element={<NotFoundPage/>} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ my-blog-temp/src/pages/AboutPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <>
+      <h1>About me</h1>
+      <p>
+        I am just a simple man trying to learn how to make React web applications.
+      </p>
+      <p>
+        I will be updating this page for probably only a short amount of time, then leave it to collect dust while learning more things.
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
